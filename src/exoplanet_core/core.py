fa77f500d9fad3cb5076bf53ef5bccd898995852
Dan F-M <foreman.mackey@gmail.com>
2020-12-17 21:34:15 -0500

    adding light curve test

--- src/exoplanet_core/core.py
+++ src/exoplanet_core/core.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__all__ = ["quad_limbdark_light_curve"]
+__all__ = ["get_cl", "quad_limbdark_light_curve"]
 
 
 import numpy as np
@@ -8,13 +8,18 @@ import numpy as np
 from .numpy import ops
 
 
-def quad_limbdark_light_curve(u1, u2, b, r):
+def get_cl(u1, u2):
     c = np.asarray(
         [1 - u1 - 1.5 * u2, u1 + 2 * u2, -0.25 * u2],
         dtype=np.float64,
         order="C",
     )
     c /= np.pi * (c[0] + c[1] / 1.5)
+    return c
+
+
+def quad_limbdark_light_curve(u1, u2, b, r):
+    c = get_cl(u1, u2)
     b = np.asarray(b, dtype=np.float64, order="C")
     r = np.asarray(r, dtype=np.float64, order="C")
     return np.dot(ops.quad_solution_vector(b, r), c) - 1
---
+++ tests/core_test.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pytest
+
+from exoplanet_core import core
+
+try:
+    import starry
+except ImportError:
+    starry = None
+
+try:
+    import batman
+except ImportError:
+    batman = None
+
+
+@pytest.mark.skipif(starry is None, reason="starry is not installed")
+def test_compare_starry():
+    u1 = 0.2
+    u2 = 0.3
+    b = np.linspace(-1.5, 1.5, 100)
+    r = 0.1 + np.zeros_like(b)
+
+    m = starry.Map(udeg=2)
+    m[1:] = [u1, u2]
+    expected = m.flux(xo=b, ro=r[0]).eval() - 1
+
+    computed = core.quad_limbdark_light_curve(u1, u2, b, r)
+    np.testing.assert_allclose(computed, expected)
+
+
+@pytest.mark.skipif(batman is None, reason="batman is not installed")
+def test_compare_batman():
+    u1 = 0.2
+    u2 = 0.3
+    b = np.linspace(-1.5, 1.5, 100)
+    r = 0.1 + np.zeros_like(b)
+
+    expected = batman._quadratic_ld._quadratic_ld(b, r[0], u1, u2, 1) - 1
+
+    computed = core.quad_limbdark_light_curve(u1, u2, b, r)
+    np.testing.assert_allclose(computed, expected, atol=1e-8)
