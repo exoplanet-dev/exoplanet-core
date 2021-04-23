a2bd8caa4fd08eff64dac70da8a9502df1dfb410
Dan F-M <foreman.mackey@gmail.com>
2021-04-23 08:37:27 -0400

    more words in the readme and handling broadcasting

--- src/exoplanet_core/core.py
+++ src/exoplanet_core/core.py
@@ -4,21 +4,31 @@ __all__ = ["quad_limbdark_light_curve", "kepler"]
 
 import numpy as np
 
-from .numpy.ops import kepler, quad_solution_vector
+from .numpy import ops
 
 
 def get_cl(u1, u2):
-    c = np.asarray(
-        [1 - u1 - 1.5 * u2, u1 + 2 * u2, -0.25 * u2],
-        dtype=np.float64,
-        order="C",
-    )
+    c = np.array([1 - u1 - 1.5 * u2, u1 + 2 * u2, -0.25 * u2])
     c /= np.pi * (c[0] + c[1] / 1.5)
     return c
 
 
+def custom_broadcast_arrays(*arrays):
+    b = np.broadcast(*arrays)
+    return tuple(
+        np.ascontiguousarray(np.broadcast_to(a, b.shape), dtype=np.float64)
+        for a in arrays
+    )
+
+
 def quad_limbdark_light_curve(u1, u2, b, r):
-    c = get_cl(u1, u2)
-    b = np.asarray(b, dtype=np.float64, order="C")
-    r = np.asarray(r, dtype=np.float64, order="C")
-    return np.dot(quad_solution_vector(b, r), c) - 1
+    b, r = custom_broadcast_arrays(b, r)
+    c = get_cl(float(u1), float(u2))
+    return np.dot(ops.quad_solution_vector(b, r), c) - 1
+
+
+def kepler(mean_anomaly, eccentricity):
+    mean_anomaly, eccentricity = custom_broadcast_arrays(
+        mean_anomaly, eccentricity
+    )
+    return ops.kepler(mean_anomaly, eccentricity)
